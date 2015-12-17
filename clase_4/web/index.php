aea36c84b2630838551fe6128322e28946d97733
Amed Ibanez <amed.nin@gmail.com>
2015-12-16 23:52:00 -0400

    Silex Blogs example 2

--- clase_4/web/index.php
+++ clase_4/web/index.php
@@ -1,5 +1,9 @@
 <?php
 
+echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">';
+
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>';
+
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -11,15 +15,20 @@ $blogPosts = array(
     1 => array(
         'date'      => '2011-03-29',
         'author'    => 'igorw',
-        'title'     => 'Using Silex',
-        'body'      => '...',
+        'title'     => 'Usando Silex',
+        'body'      => 'Blog que habla sobre silex y sus bondades....',
     ),
 );
 
 $app->get('/blog', function () use ($blogPosts) {
-    $output = '';
+    $output = '<h1>Blog</h1>';
     foreach ($blogPosts as $post) {
-        $output .= $post['title'];
+        $output .= "<div class=\"jumbotron\">
+                      <h2>{$post['title']}</h2>
+                      <p>{$post['body']}</p>
+                      <p><span class='glyphicon glyphicon-user'></span> {$post['author']}</p>
+                      <p><a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Comentar</a></p>
+                    </div>";
         $output .= '<br />';
     }
 
