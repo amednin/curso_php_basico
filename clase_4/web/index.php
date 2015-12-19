4a152ab23c567cc98bd9a1508d3699632c04d5e1
jhamil Conaca <jhamil.conaca@supersoftinc.com>
2015-12-19 10:44:56 -0400

    Usando Twig

--- clase_4/web/index.php
+++ clase_4/web/index.php
@@ -1,9 +1,5 @@
 <?php
 
-echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">';
-
-echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>';
-
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -22,27 +18,20 @@ $blogPosts = array(
         'author'    => 'igorw',
         'title'     => 'Usando Silex',
         'body'      => 'Blog que habla sobre silex y sus bondades....',
+    ),2 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'carlos',
+        'title'     => 'Usando Silex',
+        'body'      => 'Blog que habla sobre silex y sus bondades....',
     ),
 );
 
-$app->get('/blog', function () use ($blogPosts) {
-    $output = '<h1>Blog</h1>';
-    foreach ($blogPosts as $post) {
-        $output .= "<div class=\"jumbotron\">
-                      <h2>{$post['title']}</h2>
-                      <p>{$post['body']}</p>
-                      <p><span class='glyphicon glyphicon-user'></span> {$post['author']}</p>
-                      <p><a class=\"btn btn-primary btn-lg\" href=\"#\" role=\"button\">Comentar</a></p>
-                    </div>";
-        $output .= '<br />';
-    }
+$app->get('/blog', function () use ($app, $blogPosts) {
 
-    return $output;
-
-//    return $app['twig']->render('index.twig',
-//        array(
-//            'blogs' => $blogPosts
-//        ));
+    return $app['twig']->render('blogs.twig',
+        array(
+            'blogs' => $blogPosts
+        ));
 });
 
 $app->run();
